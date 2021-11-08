5b5d1f9c050e9fbb5e70e6463c9f49ec450b3d47
Raja Durai M <84493140+Raja58@users.noreply.github.com>
2021-11-08 16:36:44 +0530

    Add files via upload

---
+++ min number of jumps.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+using namespace std;
+
+int minNumberOfJumps(vector<int> array) {
+  if(array.size() == 1)
+		return 0;
+	int steps = array[0], maxReach = array[0], jumps = 0;
+	for(int i = 1; i < array.size() - 1; i++)
+	{
+		maxReach = max(maxReach, i + array[i]);
+		steps--;
+		if(steps == 0)
+		{
+			jumps++;
+			if(maxReach <= i)
+				return -1;
+			steps = maxReach - i;
+		}
+	}
+  return jumps + 1;
+}
